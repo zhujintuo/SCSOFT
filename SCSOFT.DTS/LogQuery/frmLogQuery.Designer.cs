4088091ff8525541ef21b5a2fd289051c32a190f
zhujintuo <942408056@qq.com>
2016-04-25 19:03:21 +0800

    Add LogQuery

---
+++ SCSOFT.DTS/LogQuery/frmLogQuery.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace LogQuery
+{
+    partial class frmLogQuery
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogQuery));
+            this.tb_Content = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_Key = new System.Windows.Forms.TextBox();
+            this.btn_Query = new System.Windows.Forms.Button();
+            this.tb_LogPath = new System.Windows.Forms.TextBox();
+            this.btn_LogLoad = new System.Windows.Forms.Button();
+            this.tb_Exists = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tb_Content
+            // 
+            this.tb_Content.Location = new System.Drawing.Point(12, 65);
+            this.tb_Content.Multiline = true;
+            this.tb_Content.Name = "tb_Content";
+            this.tb_Content.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tb_Content.Size = new System.Drawing.Size(662, 260);
+            this.tb_Content.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "关键字";
+            // 
+            // tb_Key
+            // 
+            this.tb_Key.Location = new System.Drawing.Point(59, 12);
+            this.tb_Key.Name = "tb_Key";
+            this.tb_Key.Size = new System.Drawing.Size(505, 21);
+            this.tb_Key.TabIndex = 1;
+            // 
+            // btn_Query
+            // 
+            this.btn_Query.Location = new System.Drawing.Point(570, 11);
+            this.btn_Query.Name = "btn_Query";
+            this.btn_Query.Size = new System.Drawing.Size(100, 23);
+            this.btn_Query.TabIndex = 2;
+            this.btn_Query.Text = "关键字查询";
+            this.btn_Query.UseVisualStyleBackColor = true;
+            this.btn_Query.Click += new System.EventHandler(this.btn_Query_Click);
+            // 
+            // tb_LogPath
+            // 
+            this.tb_LogPath.Location = new System.Drawing.Point(59, 38);
+            this.tb_LogPath.Name = "tb_LogPath";
+            this.tb_LogPath.Size = new System.Drawing.Size(505, 21);
+            this.tb_LogPath.TabIndex = 4;
+            // 
+            // btn_LogLoad
+            // 
+            this.btn_LogLoad.Location = new System.Drawing.Point(570, 36);
+            this.btn_LogLoad.Name = "btn_LogLoad";
+            this.btn_LogLoad.Size = new System.Drawing.Size(100, 23);
+            this.btn_LogLoad.TabIndex = 5;
+            this.btn_LogLoad.Text = "日志文件加载";
+            this.btn_LogLoad.UseVisualStyleBackColor = true;
+            this.btn_LogLoad.Click += new System.EventHandler(this.btn_LogLoad_Click);
+            // 
+            // tb_Exists
+            // 
+            this.tb_Exists.Location = new System.Drawing.Point(12, 332);
+            this.tb_Exists.Multiline = true;
+            this.tb_Exists.Name = "tb_Exists";
+            this.tb_Exists.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tb_Exists.Size = new System.Drawing.Size(662, 123);
+            this.tb_Exists.TabIndex = 7;
+            // 
+            // frmLogQuery
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(686, 466);
+            this.Controls.Add(this.tb_Exists);
+            this.Controls.Add(this.btn_LogLoad);
+            this.Controls.Add(this.tb_LogPath);
+            this.Controls.Add(this.btn_Query);
+            this.Controls.Add(this.tb_Key);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_Content);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmLogQuery";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "日志查询";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_Content;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_Key;
+        private System.Windows.Forms.Button btn_Query;
+        private System.Windows.Forms.TextBox tb_LogPath;
+        private System.Windows.Forms.Button btn_LogLoad;
+        private System.Windows.Forms.TextBox tb_Exists;
+    }
+}
+
---
+++ SCSOFT.DTS/LogQuery/frmLogQuery.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LogQuery
+{
+    public partial class frmLogQuery : Form
+    {
+        public frmLogQuery()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Query_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //string key = tb_Key.Text;
+                //if (string.IsNullOrEmpty(key))
+                //{
+                //    MessageBox.Show("关键字不能为空！", "提示", MessageBoxButtons.OK);
+                //    return;
+                //}
+
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine(@"select PsCode,DGICode,DGImn ");
+                sb.AppendLine(@"   from T_INFO_DGIINFO y");
+                sb.AppendLine(@"  where exists(select PsCode,DGICode ");
+                sb.AppendLine(@"                 from (select PSCode,DGICode");
+                sb.AppendLine(@"						 from T_INFO_DGIOUTPUTMAP x");
+                sb.AppendLine(@"						where 1=1");
+                sb.AppendLine(@"						  and exists(select 1 ");
+                sb.AppendLine(@"									   from V_Output ");
+                sb.AppendLine(@"									  where AttentiondegreeCode in (3,4,5)");
+                sb.AppendLine(@"										and PsCode=x.PSCode");
+                sb.AppendLine(@"										and outputcode=x.OutputCode");
+                sb.AppendLine(@"									  )");
+                sb.AppendLine(@"					   ) d");
+                sb.AppendLine(@"			    where d.PSCode=y.PsCode ");
+                sb.AppendLine(@"				  and d.DGICode=y.DGICode");
+                sb.AppendLine(@"			   )");
+
+                DataSet ds = res.code.DbHelperSQL.Query(sb.ToString());
+                if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+                {
+                    sb = new StringBuilder();
+                    foreach (DataRow row in ds.Tables[0].Rows)
+                    {
+                        if (tb_Content.Text.Contains(row["DGImn"].ToString()))
+                        {
+                            sb.AppendFormat("{0} {1} {2} \n", row["PsCode"].ToString(), row["DGICode"].ToString(), row["DGImn"].ToString());
+                        }
+                    }
+                    tb_Exists.Text = sb.ToString();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("数据加载异常！异常信息为：" + ex.Message, "提示", MessageBoxButtons.OK);
+            }
+        }
+
+        private void btn_LogLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = true;
+            fileDialog.Title = "请选择文件";
+            fileDialog.Filter = "TXT文件(*.txt)|*.txt";
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string file = fileDialog.FileName;
+                tb_LogPath.Text = file;
+
+                try
+                {
+                    StreamReader sr = new StreamReader(file, System.Text.Encoding.Default);
+                    StringBuilder sb = new StringBuilder();
+                    while (sr.ReadLine() != null)
+                    {
+                        sb.AppendLine(sr.ReadLine());
+                    }
+                    tb_Content.Text = sb.ToString();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("数据加载异常！异常信息为：" + ex.Message, "提示", MessageBoxButtons.OK);
+                }
+            }
+        }
+    }
+}
